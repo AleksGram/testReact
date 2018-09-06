98cfbee789dc78d92b29723f6ea0a2c8e53d3a0e
AlexGram <agramatik1@gmail.com>
2018-09-07 00:17:44 +0300

    added enzyme

--- testreact/src/components/__tests__/App.test.js
+++ testreact/src/components/__tests__/App.test.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from '../App';
+import {shallow} from 'enzyme';
+import CommentBox from "../CommentBox";
 
 
 it('shows comments box', () => {
-    const div = document.createElement('div');
+   const wrapped = shallow(<App/>);
 
-    ReactDOM.render(<App/>, div);
-
-    expect(div.innerHTML).toContain('Comment Box');
-    ReactDOM.unmountComponentAtNode(div);
+   expect(wrapped.find(CommentBox).length).toEqual(1);
 });
