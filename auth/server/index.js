37dd6c1726601f91f8d99872c89b5f958b7b6bd4
agra <agramatik@spotoption.com>
2018-10-03 17:33:04 +0300

    start reating server

--- auth/server/index.js
+++ auth/server/index.js
@@ -1,24 +1,18 @@
-// Main starting point of the application
+
 const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
 const router = require('./router');
-const mongoose = require('mongoose');
-const cors = require('cors');
 
-// DB Setup
-mongoose.connect('mongodb://localhost/auth');
-
-// App Setup
-app.use(morgan('combined'));
-app.use(cors());
-app.use(bodyParser.json({ type: '*/*' }));
+// App setup
+app.use(morgan('combined')); //login incoming requests using for debugging
+app.use(bodyParser.json({type: '*/*'}));
 router(app);
 
-// Server Setup
+//Server setup
 const port = process.env.PORT || 3090;
 const server = http.createServer(app);
 server.listen(port);
-console.log('Server listening on:', port);
+console.log('Server listen on: ', port);
--- testreact/src/middlewares/stateValidator.js
+++ testreact/src/middlewares/stateValidator.js
@@ -1,4 +1,4 @@
-import tv4 from 'tv4';
+import tv4 from 'tv4'; //library for jsonscheme
 import stateSchema from './stateSchema';
 
 export default ({ dispatch, getState }) => (next) => (action) => {
