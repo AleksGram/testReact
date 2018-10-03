eaea8ceed050517e381453635dc4a78999242539
AlexGram <agramatik1@gmail.com>
2018-10-04 01:13:40 +0300

    connecting mongoDB

--- auth/server/index.js
+++ auth/server/index.js
@@ -5,6 +5,10 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
 const router = require('./router');
+const mongoose = require('mongoose');
+
+//DB Setup
+mongoose.connect('mongodb://localhost:auth/auth', { useNewUrlParser: true });
 
 // App setup
 app.use(morgan('combined')); //login incoming requests using for debugging
